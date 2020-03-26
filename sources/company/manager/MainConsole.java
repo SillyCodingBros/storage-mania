ed72b52678a96a3205829dfc54a98d93d9ae2623
BruhP8 <luc.biohazard@gmail.com>
2020-03-26 17:15:36 +0100

    Main start with commands

--- sources/company/manager/MainConsole.java
+++ sources/company/manager/MainConsole.java
@@ -1,5 +1,7 @@
 package company.manager;
 
+import java.util.Scanner;
+
 import company.data.*;
 
 /**
@@ -75,14 +77,23 @@ public class MainConsole
 	}
 
 	public static void main(String[] args) {
-		//Stock stock = new Stock();
-		//stock.toString();
+		Stock stock = new Stock();
+		String input;
+		String help = "Avalable commands are : \n\t exit : stops the machine, \n\t help : displays this message, \n";
 		//printOrders();
-		//printHelp();
+		System.out.println(help);
 		while(true){
-
+			Scanner reader = new Scanner(System.in);
+			System.out.print(">");
+			input = reader.nextLine();
+			if(input.equalsIgnoreCase("exit"))
+				break;
+			else if(input.equalsIgnoreCase("help"))
+				System.out.println(help);
+			else{
+				System.out.println("Unknown command, type 'help' to see supported commands.");
+			}
 		}
 	}
-	
 }
 
